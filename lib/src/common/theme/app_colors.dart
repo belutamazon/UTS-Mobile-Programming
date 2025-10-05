59ab9befd6396e39ef769e0dc9fe8ddb944f9d20
belutamazon <f.alvitio112@gmail.com>
2025-10-05 19:56:36 +0700

    Delete lib/src/common directory
    salah commit

--- lib/src/common/theme/app_colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color background = Color(0xFF000000); // Hitam pekat
-  static const Color primaryText = Color(0xFFE7E9EA); // Putih gading
-  static const Color secondaryText = Color(0xFF71767B); // Abu-abu untuk handle & timestamp
-  static const Color accent = Color(0xFF1D9BF0); // Biru khas X
-  static const Color divider = Color(0xFF2F3336); // Warna garis pemisah
-}
